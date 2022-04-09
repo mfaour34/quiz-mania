a7d109b1d43df4d3c27673c8172654e97f83cd4f
mfaour34 <faour606@gmail.com>
2022-04-09 16:06:10 +0300

    make username and email unqiue

--- src/modules/user/infrastructure/schemas/User.ts
+++ src/modules/user/infrastructure/schemas/User.ts
@@ -2,8 +2,8 @@ import { IUser } from '@user/application/entities/IUser'
 import { Schema, model } from 'mongoose'
 
 const userSchema = new Schema<IUser>({
-  email: { type: String, required: true },
-  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 })
 
